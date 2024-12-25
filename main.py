4a19c3015e2b63e742420d95e137e70b7d4ea767
RudraSalunkhe <rudrasalunkhe61@gmail.com>
2024-12-25 18:52:54 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+age=int(input("enter your age ="))
+if age >20:
+    print("you are not allowed")
+else :
+    if age<10 :
+        print("you are not allowed")
+    else:
+        print("you are allowed")
